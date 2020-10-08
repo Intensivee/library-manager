a60aed0c597d93f7c015e15f167a6bee53b04d0e
intensivee <mrorange016@gmail.com>
2020-10-08 15:53:17 +0200

    #1 author-details db-readed categories

--- client/src/app/components/author-details/author-details.component.ts
+++ client/src/app/components/author-details/author-details.component.ts
@@ -1,3 +1,4 @@
+import { CategoryService } from './../../service/category.service';
 import { BookService } from './../../service/book.service';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
@@ -5,6 +6,9 @@ import { AuthorService } from './../../service/author.service';
 import { Component, OnInit } from '@angular/core';
 import { Author } from 'src/app/models/author';
 import { Book } from 'src/app/models/book';
+import { mergeMap, map } from 'rxjs/operators';
+import { forkJoin } from 'rxjs';
+import { moveEmitHelpers } from 'typescript';
 
 @Component({
   selector: 'app-author-details',
@@ -17,23 +21,21 @@ export class AuthorDetailsComponent implements OnInit {
   books: Book[];
 
   constructor(private authorService: AuthorService,
-              private bookService: BookService,
-              private route: ActivatedRoute) { }
+    private bookService: BookService,
+    private categoryService: CategoryService,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.getAuthor();
   }
 
   getAuthor(): void {
-    if (this.route.snapshot.paramMap.has('id')){
+    if (this.route.snapshot.paramMap.has('id')) {
       const authorId = +this.route.snapshot.paramMap.get('id');
-      this.authorService.getAuthorById(authorId).subscribe(
-        data => {
-          this.author = data;
-        });
-      this.bookService.getBooksByAuthorId(authorId).subscribe(
-        data => this.books = data);
+      this.authorService.getAuthorById(authorId).subscribe(author => this.author = author);
+
+      this.bookService.getBooksByAuthorId(authorId).subscribe(books => this.books = books);
+      console.log(this.books);
     }
   }
-
 }
--- client/src/app/service/book.service.ts
+++ client/src/app/service/book.service.ts
@@ -1,21 +1,37 @@
-import { Observable } from 'rxjs';
+import { CategoryService } from './category.service';
+import { forkJoin, Observable } from 'rxjs';
 import { API_URL } from '../app.constants';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Book } from '../models/book';
-import { map } from 'rxjs/operators';
+import { map, mergeMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private categoryService: CategoryService) { }
 
   getBooksByAuthorId(id: number): Observable<Book[]> {
     const url = `${API_URL}/books/search/findByAuthorId?id=${id}`;
     return this.http.get<UnwrapResponse>(url).pipe(
-      map(response => response._embedded.books));
+      map(response => response._embedded.books)).pipe(
+        mergeMap( books => {
+          return forkJoin(
+            books.map(book => {
+              return this.categoryService.getCategoriesByBookId(book.id)
+              .pipe(
+                map(categories => {
+                  book.categories = categories;
+                  return book;
+                })
+              );
+            })
+          );
+        })
+      );
   }
 
 }
